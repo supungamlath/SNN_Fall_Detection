2e5dd7dd3d8f8404acfe7d819642be9681682565
Supun Gamlath <86224008+supungamlath@users.noreply.github.com>
2024-02-11 22:58:49 +0530

    Add data labelling functions

---
+++ utils/data_labelling.py
@@ -0,0 +1,30 @@
+import os
+import pandas as pd
+
+
+def label_URFD_dataset():
+    dataset_folder = "./data/urfd-spiking-dataset-240"
+    folder_names = [
+        folder
+        for folder in os.listdir(dataset_folder)
+        if os.path.isdir(os.path.join(dataset_folder, folder))
+    ]
+    labels = [1 if "fall" in name else 0 for name in folder_names]
+    df = pd.DataFrame({"folder_name": folder_names, "label": labels})
+    df.to_csv(f"{dataset_folder}/labels.csv", index=False)
+
+
+def label_HAR_UP_dataset():
+    dataset_folder = "./data/har-up-spiking-dataset-240"
+    folder_names = [
+        folder
+        for folder in os.listdir(dataset_folder)
+        if os.path.isdir(os.path.join(dataset_folder, folder))
+    ]
+    labels = [1] * len(folder_names)
+    df = pd.DataFrame({"folder_name": folder_names, "label": labels})
+    df.to_csv(f"{dataset_folder}/labels.csv", index=False)
+
+
+# label_URFD_dataset()
+# label_HAR_UP_dataset()
