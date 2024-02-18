1f6a8731e67e3f1f67b11ceae635dd0123e9ad25
Supun Gamlath <86224008+supungamlath@users.noreply.github.com>
2024-02-18 12:05:34 +0530

    Add color mapping to voltage traces and live plot functionality

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,15 +1,21 @@
-from matplotlib import pyplot as plt
+from matplotlib import cm, pyplot as plt
 import numpy as np
 import seaborn as sns
 from matplotlib.gridspec import GridSpec
+import matplotlib.colors as mcolors
 import plotly.graph_objs as go
 from plotly.offline import iplot
 
 label_names = {0: "Non-Fall", 1: "Fall"}
 
 
-def plot_voltage_traces(mem, spk=None, labels=None, dim=(1, 7), spike_height=5):
-    plt.figure(figsize=(14, 2))
+def plot_voltage_traces(
+    mem, diff=None, spk=None, labels=None, dim=(1, 7), spike_height=5, renderer=None
+):
+    cmap = cm.get_cmap("RdYlGn")  # Red to Yellow to Green colormap
+    norm = mcolors.Normalize(vmin=0, vmax=60)
+
+    fig = plt.figure(figsize=(14, 2))
     gs = GridSpec(*dim)
 
     if spk is not None:
@@ -25,30 +31,38 @@ def plot_voltage_traces(mem, spk=None, labels=None, dim=(1, 7), spike_height=5):
             ax = plt.subplot(gs[i], sharey=a0)
 
         ax.plot(dat[i])
+        ax.set_facecolor(cmap(norm(diff[i])))
         if labels is not None:
             ax.text(
                 0.5,
                 -0.5,
-                label_names[labels[i]],
+                f"Pred: {labels[i]} Conf: {diff[i]:.2f}",
                 transform=ax.transAxes,
                 ha="center",
                 va="center",
             )
         # ax.axis("off")
     plt.tight_layout()
-    plt.show()
+    if renderer:
+        renderer(fig)
+    else:
+        plt.show()
 
 
-def live_plot(loss):
-    if len(loss) == 1:
+def live_plot(np_arr, title="", y_label="Loss", renderer=None):
+    if len(np_arr) == 1:
         return
-    ax = plt.figure(figsize=(3, 2), dpi=150).gca()
-    ax.plot(range(1, len(loss) + 1), loss)
+    plt.title(title)
+    fig, ax = plt.subplots(figsize=(3, 2))
+    ax.plot(range(1, len(np_arr) + 1), np_arr)
     ax.set_xlabel("Epoch")
-    ax.set_ylabel("Loss")
+    ax.set_ylabel(y_label)
     ax.xaxis.get_major_locator().set_params(integer=True)
     sns.despine()
-    plt.show()
+    if renderer:
+        renderer(fig, use_container_width=False)
+    else:
+        plt.show()
 
 
 def plot_neuron_activity(data, title, x_title, y_title):
