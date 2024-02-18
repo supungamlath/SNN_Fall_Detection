b75f67e5ca1e1400c2b1b599a71270c03a121451
Supun Gamlath <86224008+supungamlath@users.noreply.github.com>
2024-02-18 22:29:59 +0530

    Add SpikingDataLoader and SpikingDataset classes

---
+++ utils/SpikingDataLoader.py
@@ -0,0 +1,48 @@
+import numpy as np
+import torch
+from torch.utils.data import DataLoader
+
+
+# Custom DataLoader that uses a sparse_data_generator as the collate function
+class SpikingDataLoader(DataLoader):
+    def __init__(self, *args, **kwargs):
+        dataset = args[0]
+        self.nb_units = dataset.nb_pixels
+        self.max_time = dataset.max_time
+        self.nb_steps = dataset.nb_steps
+        self.frame_width = dataset.frame_width
+        self.batch_size = kwargs["batch_size"]
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print("Initializing DataLoader using device:", self.device)
+
+        kwargs["collate_fn"] = self.sparse_data_generator
+        super().__init__(*args, **kwargs)
+
+    def sparse_data_generator(
+        self,
+        batch,
+    ):
+        time_bins = np.linspace(0, self.max_time, num=self.nb_steps)
+
+        coo = [[] for _ in range(3)]
+        labels = []
+        for i, datapoint in enumerate(batch):
+            sample, label = datapoint
+            times = np.digitize(sample["timestamp"], time_bins)
+            units = sample["y"] * self.frame_width + sample["x"]
+            batch = [i for _ in range(len(times))]
+
+            coo[0].extend(batch)
+            coo[1].extend(times)
+            coo[2].extend(units)
+            labels.append(label)
+
+        i = torch.LongTensor(coo).to(self.device)
+        v = torch.FloatTensor(np.ones(len(coo[0]))).to(self.device)
+
+        X_batch = torch.sparse_coo_tensor(
+            i, v, torch.Size([self.batch_size, self.nb_steps, self.nb_units])
+        ).to(self.device)
+        y_batch = torch.tensor(labels).to(self.device)
+
+        return X_batch.to(device=self.device), y_batch.to(device=self.device)
---
+++ utils/SpikingDataset.py
@@ -0,0 +1,107 @@
+import os
+import numpy as np
+import pandas as pd
+from torch.utils.data import Dataset
+import h5py
+from sklearn.model_selection import train_test_split
+
+
+class SpikingDataset(Dataset):
+    def __init__(
+        self,
+        root_dir,
+        max_time=15.0,
+        nb_steps=1000,
+        read_csv=True,
+    ):
+        self.root_dir = root_dir
+        self.max_time = max_time
+        self.nb_steps = nb_steps
+        self.frame_height = 180
+        self.frame_width = 240
+        self.max_timestamp = max_time * 1e6
+        self.nb_pixels = self.frame_height * self.frame_width
+
+        if read_csv:
+            # Load the CSV file into a DataFrame
+            labels_file = os.path.join(root_dir, "labels.csv")
+            df = pd.read_csv(labels_file)
+
+            # Convert the columns to lists
+            self.folder_names = df["folder_name"].tolist()
+            self.labels = df["label"].tolist()
+
+    def __len__(self):
+        return len(self.folder_names)
+
+    def __getitem__(self, idx):
+        events_file = self.folder_names[idx] + ".h5"
+        spike_data = h5py.File(
+            os.path.join(self.root_dir, self.folder_names[idx], events_file), "r"
+        )
+        spike_tuples = np.array(spike_data["events"])
+        # Remove events that occur after max_timestamp
+        spike_tuples = spike_tuples[spike_tuples[:, 0] < self.max_timestamp]
+        sample = {
+            "timestamp": spike_tuples[:, 0] / 1e6,
+            "x": spike_tuples[:, 1],
+            "y": spike_tuples[:, 2],
+            "polarity": spike_tuples[:, 3],
+        }
+        return sample, self.labels[idx]
+
+    def edit_label(self, idx, label):
+        self.labels[idx] = label
+
+    def get_folder_name(self, idx):
+        return self.folder_names[idx]
+
+    def get_label(self, idx):
+        return self.labels[idx]
+
+    def get_video_path(self, idx):
+        video_path = os.path.join(
+            self.root_dir, self.folder_names[idx], "dvs-video.avi"
+        )
+        return os.path.normpath(video_path)
+
+    def random_split(self, test_size=0.25, shuffle=True):
+        train_dataset = SpikingDataset(
+            root_dir=self.root_dir,
+            max_time=self.max_time,
+            nb_steps=self.nb_steps,
+            read_csv=False,
+        )
+        test_dataset = SpikingDataset(
+            root_dir=self.root_dir,
+            max_time=self.max_time,
+            nb_steps=self.nb_steps,
+            read_csv=False,
+        )
+
+        (
+            train_dataset.folder_names,
+            test_dataset.folder_names,
+            train_dataset.labels,
+            test_dataset.labels,
+        ) = train_test_split(
+            self.folder_names,
+            self.labels,
+            test_size=test_size,
+            shuffle=shuffle,
+            random_state=42,
+        )
+
+        return train_dataset, test_dataset
+
+    def save_labels(self):
+        df = pd.DataFrame(
+            {
+                "folder_name": self.folder_names,
+                "label": self.labels,
+            }
+        )
+        df.to_csv(
+            os.path.join(self.root_dir, "labels.csv"),
+            index=False,
+        )
