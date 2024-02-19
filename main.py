c795ecfca5333c98ea4bde456ecd98f4a37cb66b
Supun Gamlath <86224008+supungamlath@users.noreply.github.com>
2024-02-19 15:57:20 +0530

    Refactor code

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+import os
 import torch
-from utils.SpikingDataLoader import SpikingDataLoader
 from utils.SpikingDataset import SpikingDataset
+from utils.SpikingDataLoader import SpikingDataLoader
 
 from models.model import SNN
 
 from utils.Trainer import Trainer
 
+os.environ["root_folder"] = "."
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device:", device)
 
 dataset = SpikingDataset(
-    root_dir="./data/urfd-spiking-dataset-240", max_time=15.0, nb_steps=1000
+    root_dir=f"{os.environ['root_folder']}/data/urfd-spiking-dataset-240",
+    max_time=15.0,
+    nb_steps=1000,
 )
 
 # Splitting the dataset
@@ -32,22 +36,19 @@ train_loader = SpikingDataLoader(train_dataset, batch_size=7, shuffle=True)
 test_loader = SpikingDataLoader(test_dataset, batch_size=7, shuffle=False)
 
 # Load the model
-model = torch.load("./models/saved/model_v5.pth").to(device)
+model = torch.load(f"{os.environ['root_folder']}/models/saved/model_v5.pth").to(device)
 # model.eval()
 
 # Train the model
 trainer = Trainer(model=model)
-# trainer.train(train_loader, nb_epochs=5, lr=2e-4)
+trainer.train(train_loader, nb_epochs=5, lr=2e-4)
 
 # Save the model
-# torch.save(model, "./models/saved/model_v7.pth")
+torch.save(model, f"{os.environ['root_folder']}/models/saved/model_v7.pth")
 
 # Evaluate the model
-# print("Training accuracy:", trainer.compute_accuracy(train_loader))
-# print("Test accuracy:", trainer.compute_accuracy(test_loader))
-
-# x, y = dataset.get_samples()
-# print("Validation accuracy:", trainer.compute_accuracy(x, y))
+print("Training accuracy:", trainer.compute_accuracy(train_loader))
+print("Test accuracy:", trainer.compute_accuracy(test_loader))
 
 # Visualize the output spikes
 trainer.visualize_output(test_loader, 1)
