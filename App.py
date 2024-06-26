4215ca8fa9d9756baf7a660a8d942ecfcfbc7e94
Supun Gamlath <86224008+supungamlath@users.noreply.github.com>
2024-06-26 22:41:26 +0530

    Update app start page

--- App.py
+++ App.py
@@ -11,22 +11,20 @@ st.set_page_config(
     page_icon="🧊",
 )
 
-st.write("# Welcome to Streamlit! 👋")
+st.write("# Welcome to FallSNN Dashboard")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
+    This interactive dashboard is designed to demonstrate the capabilities of Spiking Neural Networks (SNNs) in detecting falls, a critical public health issue that predominantly affects older adults. According to the World Health Organization, falls are a leading cause of fatal injuries among the elderly, with an estimated 684,000 fatalities annually. Prompt detection and response to falls are crucial for preventing severe injuries and improving outcomes.
+
+    This approach to fall detection leverages the unique properties of Spiking Neural Networks. Unlike traditional computer vision techniques that rely on handcrafted features and are often limited by environmental variables such as lighting and occlusions, SNNs offer a dynamic and robust solution. They mimic the human brain's processing method, using discrete spikes to encode information and efficiently handle temporal data, making them particularly suitable for real-time fall detection in varied settings.
+
+    ## Features of the Dashboard:
+    - **Inspect Datasets:** Explore the datasets used for training the fall detection models.
+    - **Create or Load Model:** Start from scratch with a new model and parameters or load a pre-trained model.
+    - **Train Model:** Configure and run training sessions with real-time updates on progress.
+    - **Evaluate Predictions:** Test the model's predictions using on video data and observe how well the model detects falls.
+    - **Analyse Metrics:** Review detailed metrics such as accuracy, and loss to evaluate the effectiveness of the model.
+
+    """
 )
