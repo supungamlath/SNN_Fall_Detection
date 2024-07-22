ffd0e9644b7f54106d0544fd669e3c02eff8ad11
Supun Gamlath <86224008+supungamlath@users.noreply.github.com>
2024-07-22 21:20:15 +0530

    Refactor analysis page plots and add options for more metrics

--- pages/5_Analysis.py
+++ pages/5_Analysis.py
@@ -39,57 +39,68 @@ if model_names:
 if datasets:
     df = df[df["dataset"].isin(datasets)]
 
+# Legend selection option
+legends = {
+    "Model Name": "model_name",
+    "Dataset": "dataset",
+    "Memory Time Constant": "tau_mem",
+    "Synaptic Time Constant": "tau_syn",
+    "Layer Sizes": "snn_layers",
+}
+legend_option = st.selectbox("Select Legend for Plots", legends.keys())
+
 # Plot selection options
-plot_loss_history = st.checkbox("Plot Train Loss History")
-plot_train_accuracy_history = st.checkbox("Plot Train Accuracy History")
-plot_test_accuracy_history = st.checkbox("Plot Test Accuracy History")
+with st.expander("Select Plots to Display", expanded=True):
+    cols = st.columns(2)
+    with cols[0]:
+        plot_train_accuracy_history = st.checkbox("Train Set Accuracy")
+        plot_train_precision_history = st.checkbox("Train Set Precision")
+        plot_train_recall_history = st.checkbox("Train Set Recall")
+        plot_train_f1_score_history = st.checkbox("Train Set F1 Score")
+        plot_train_loss_history = st.checkbox("Train Set Loss")
+    with cols[1]:
+        plot_test_accuracy_history = st.checkbox("Test Set Accuracy")
+        plot_test_precision_history = st.checkbox("Test Set Precision")
+        plot_test_recall_history = st.checkbox("Test Set Recall")
+        plot_test_f1_score_history = st.checkbox("Test Set F1 Score")
 
-# Plot Train Loss History
-if plot_loss_history:
-    st.header("Train Loss History")
+
+# Common plot format for all plots
+def plot_metrics_format(title, list_column_name):
+    st.header(title)
     fig = px.line()
-    for model_name in df["model_name"].unique():
-        model_data = df[df["model_name"] == model_name]
+    sorted_df = df.sort_values(by=legends[legend_option])
+    for model_name in sorted_df["model_name"].unique():
+        model_data = sorted_df[sorted_df["model_name"] == model_name]
         for idx, row in model_data.iterrows():
-            if row["train_loss_hist"]:
+            if row.get(list_column_name):
                 fig.add_scatter(
-                    x=list(range(1, len(row["train_loss_hist"]) + 1)),
-                    y=row["train_loss_hist"],
+                    x=list(range(1, len(row[list_column_name]) + 1)),
+                    y=row[list_column_name],
                     mode="lines",
-                    name=f"{model_name} (Run at {row['datetime']})",
+                    name=f"{legend_option} - {row[legends[legend_option]]}",
                 )
     st.plotly_chart(fig)
 
-# Plot Train Accuracy History
+
 if plot_train_accuracy_history:
-    st.header("Train Accuracy History")
-    fig = px.line()
-    for model_name in df["model_name"].unique():
-        model_data = df[df["model_name"] == model_name]
-        for idx, row in model_data.iterrows():
-            if row["train_accuracy_hist"]:
-                fig.add_scatter(
-                    x=list(range(1, len(row["train_accuracy_hist"]) + 1)),
-                    y=row["train_accuracy_hist"],
-                    mode="lines",
-                    name=f"{model_name} (Run at {row['datetime']})",
-                )
-    st.plotly_chart(fig)
+    plot_metrics_format("Train Set Accuracy History", "train_accuracy_hist")
+if plot_train_precision_history:
+    plot_metrics_format("Train Set Precision History", "train_precision_hist")
+if plot_train_recall_history:
+    plot_metrics_format("Train Set Recall History", "train_recall_hist")
+if plot_train_f1_score_history:
+    plot_metrics_format("Train Set F1 Score History", "train_f1_score_hist")
+if plot_train_loss_history:
+    plot_metrics_format("Train Set Loss History", "train_loss_hist")
 
-# Plot Test Accuracy History
 if plot_test_accuracy_history:
-    st.header("Test Accuracy History")
-    fig = px.line()
-    for model_name in df["model_name"].unique():
-        model_data = df[df["model_name"] == model_name]
-        for idx, row in model_data.iterrows():
-            if row["test_accuracy_hist"]:
-                fig.add_scatter(
-                    x=list(range(1, len(row["test_accuracy_hist"]) + 1)),
-                    y=row["test_accuracy_hist"],
-                    mode="lines",
-                    name=f"{model_name} (Run at {row['datetime']})",
-                )
-    st.plotly_chart(fig)
+    plot_metrics_format("Test Set Accuracy History", "test_accuracy_hist")
+if plot_test_precision_history:
+    plot_metrics_format("Test Set Precision History", "test_precision_hist")
+if plot_test_recall_history:
+    plot_metrics_format("Test Set Recall History", "test_recall_hist")
+if plot_test_f1_score_history:
+    plot_metrics_format("Test Set F1 Score History", "test_f1_score_hist")
 
 st.write("Select rows and metrics to compare across different models.")
